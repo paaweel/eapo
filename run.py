d495105b9991cfcdfdf892811bf30611a2e2cd72
paaweel <pawel.gakan@gmail.com>
2021-01-30 23:35:11 +0100

    Refactor alg and log generation progress

--- run.py
+++ run.py
@@ -2,124 +2,136 @@ import random
 import json
 from deap import base, creator, tools
 from scoop import futures
+from functools import partial
+from tqdm import tqdm
+
+tqdm = partial(tqdm, position=0, leave=True)
 
 from operators import crossover_op, mutate_op, evaluateModel, random_individual
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", dict, fitness=creator.FitnessMax)
 
-toolbox = base.Toolbox()
-toolbox.register("individual", random_individual, creator.Individual)
-# define the population to be a list of individuals
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+SEED = 64
+CXPB, MUTPB = 0.5, 0.2
 
-toolbox.register("map", futures.map)
-#----------
-# Operator registration
-#----------
-toolbox.register("evaluate", evaluateModel)
-toolbox.register("mate", crossover_op)
-toolbox.register("mutate", mutate_op, low={"gamma": 0, "learning_rate": 0}, up={"gamma": 1, "learning_rate": 1})
-# operator for selecting individuals for breeding the next
-# generation: each individual of the current generation
-# is replaced by the 'fittest' (best) of three individuals
-# drawn randomly from the current generation.
-toolbox.register("select", tools.selTournament, tournsize=3)
-#----------
+class Algorithm:
+    def __init__(self, try_concurrent=True):
 
-def main():
-    random.seed(64)
-    data = []
-    loggFile = "loggings.json"
-    # create an initial population of 300 individuals (where
-    # each individual is a list of integers)
-    pop = toolbox.population(n=10)
+        random.seed(SEED)
 
-    # CXPB  is the probability with which two individuals
-    #       are crossed
-    #
-    # MUTPB is the probability for mutating an individual
-    CXPB, MUTPB = 0.5, 0.2
-    
-    print("Start of evolution")
-    
-    # Evaluate the entire population
-    fitnesses = list(map(toolbox.evaluate, pop))
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
-    
-    print("  Evaluated %i individuals" % len(pop))
-
-    # Extracting all the fitnesses of 
-    fits = [ind.fitness.values[0] for ind in pop]
-
-    # Variable keeping track of the number of generations
-    g = 0
-    
-    # Begin the evolution
-    while max(fits) < 0.9 and g < 50:
-        # A new generation
-        g = g + 1
-        print("-- Generation %i --" % g)
+        self.toolbox = base.Toolbox()
+        self.__setup_individual()
+        self.__setup_operators()       
         
-        # Select the next generation individuals
-        offspring = toolbox.select(pop, len(pop))
-        # Clone the selected individuals
-        offspring = list(map(toolbox.clone, offspring))
-    
+        if try_concurrent:
+            self.toolbox.register("map", futures.map)
+
+
+    def __generation(self):
+        offspring = self.__get_offspring()
+        self.__apply_crossover(offspring)
+        self.__apply_mutation(offspring)
+        self.__evaluate_offspring(offspring)
+
+        self.pop[:] = offspring
+        
+
+    def __apply_crossover(self, offspring):
         # Apply crossover and mutation on the offspring
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
-
             # cross two individuals with probability CXPB
             if random.random() < CXPB:
-                toolbox.mate(child1, child2)
-
+                self.toolbox.mate(child1, child2)
                 # fitness values of the children
                 # must be recalculated later
                 del child1.fitness.values
                 del child2.fitness.values
 
-        for mutant in offspring:
 
+    def __apply_mutation(self, offspring):
+        for mutant in offspring:
             # mutate an individual with probability MUTPB
             if random.random() < MUTPB:
-                toolbox.mutate(mutant)
+                self.toolbox.mutate(mutant)
                 del mutant.fitness.values
-    
+
+
+    def __evaluate_offspring(self, offspring):
         # Evaluate the individuals with an invalid fitness
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        fitnesses = self.toolbox.map(self.toolbox.evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
+
+        # print("  Evaluated %i individuals" % len(invalid_ind))
+
+
+    def __get_offspring(self):
+        # Select the next generation individuals
+        offspring = self.toolbox.select(self.pop, len(self.pop))
+        # Clone the selected individuals
+        offspring = list(map(self.toolbox.clone, offspring))
         
-        print("  Evaluated %i individuals" % len(invalid_ind))
-        
-        # The population is entirely replaced by the offspring
-        pop[:] = offspring
-        
+        return offspring
+
+
+    def __log(self):
         # Gather all the fitnesses in one list and print the stats
         fits = [ind.fitness.values[0] for ind in pop]
-        
         length = len(pop)
         mean = sum(fits) / length
         sum2 = sum(x*x for x in fits)
         std = abs(sum2 / length - mean**2)**0.5
-        
+
         print("  Min %s" % min(fits))
         print("  Max %s" % max(fits))
         print("  Avg %s" % mean)
         print("  Std %s" % std)
 
-        best_ind_gen = tools.selBest(pop, 1)[0]
 
-        data.append({"generation": g, "parameters": best_ind_gen, "fitness": best_ind_gen.fitness.values})
-        with open(loggFile, 'w') as f:
-            json.dump(data, f)
-    
-    print("-- End of (successful) evolution --")
-    
-    best_ind = tools.selBest(pop, 1)[0]
-    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+    def run(self):
+
+        self.pop = self.toolbox.population(n=12)
+
+        # pbar = tqdm(range(50))
+        for generation_num in tqdm(range(50)):
+            self.__generation()
+            # pbar.set_description("Processing gen: %s" % generation_num)
+
+            # best_ind_gen = tools.selBest(pop, 1)[0]
+
+            # data.append({"generation": g, "parameters": best_ind_gen, "fitness": best_ind_gen.fitness.values})
+            # with open(loggFile, 'w') as f:
+            #     json.dump(data, f)
+
+        
+    def __setup_individual(self):
+        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+        creator.create("Individual", dict, fitness=creator.FitnessMax)
+        
+        self.toolbox.register("individual", random_individual, creator.Individual)
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+
+    def __setup_operators(self):
+        self.toolbox.register("evaluate", evaluateModel)
+        self.toolbox.register("mate", crossover_op)
+        self.toolbox.register(
+            "mutate", mutate_op, 
+            low={
+                "gamma": 0,
+                "learning_rate": 0
+            }, 
+            up={
+                "gamma": 1,
+                "learning_rate": 1
+            })
+
+        # operator for selecting individuals for breeding the next
+        # generation: each individual of the current generation
+        # is replaced by the 'fittest' (best) of three individuals
+        # drawn randomly from the current generation.
+        self.toolbox.register("select", tools.selTournament, tournsize=3)
+
 
 if __name__ == "__main__":
-    main()
+    a = Algorithm()
+    a.run()
